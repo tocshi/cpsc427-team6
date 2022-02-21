6f48509c467d74abcb450065d679d468aff0b8d8
Jordan Dang <jordandang1@hotmail.com>
2022-02-21 01:49:34 -0800

    adjusted text shaders

--- shaders/text.fs.glsl
+++ shaders/text.fs.glsl
@@ -1,12 +1,18 @@
-#version 330 core
-in vec2 TexCoords;
-out vec4 color;
+#version 330
 
-uniform sampler2D text;
-uniform vec3 textColor;
+// From vertex shader
+in vec2 texcoord;
+
+// Application data
+uniform sampler2D sampler0;
+uniform vec3 fcolor;
+
+// Output color
+layout(location = 0) out  vec4 color;
 
 void main()
-{    
-    vec4 sampled = vec4(1.0, 1.0, 1.0, texture(text, TexCoords).r);
-    color = vec4(textColor, 1.0) * sampled;
-}  
+{
+	// color = texture(sampler0, vec2(texcoord.x, texcoord.y));
+	// color.a = 1;
+	color = vec4(fcolor, ceil(texture(sampler0, vec2(texcoord.x, texcoord.y)).r)); 
+}
--- shaders/text.vs.glsl
+++ shaders/text.vs.glsl
@@ -1,11 +1,20 @@
-#version 330 core
-layout (location = 0) in vec4 vertex; // <vec2 pos, vec2 tex>
-out vec2 TexCoords;
+#version 330
 
-uniform mat4 projection;
+// Input attributes
+in vec3 in_position;
+in vec2 in_texcoord;
+
+// Passed to fragment shader
+out vec2 texcoord;
+
+// Application data
+uniform mat3 transform;
+uniform mat3 projection;
 
 void main()
 {
-    gl_Position = projection * vec4(vertex.xy, 0.0, 1.0);
-    TexCoords = vertex.zw;
-}  
+	texcoord = in_texcoord;
+	vec3 pos = projection * transform * vec3(in_position.xy / 2, 1.0);
+	// vec3 pos = vec3(in_position.xy / 2, 1.0);
+	gl_Position = vec4(pos.xy, in_position.z, 1.0);
+}

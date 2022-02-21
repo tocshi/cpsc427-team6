d4dc992544f2c527acb3d10f904b8fa431dc7137
tocshi <50164881+tocshi@users.noreply.github.com>
2022-02-21 12:53:27 -0800

    tweaked tilemap and added tilemap image

--- data/maps/roguelikeDungeon_magenta.tsx
+++ data/maps/roguelikeDungeon_magenta.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.2" name="roguelikeDungeon_magenta" tilewidth="16" tileheight="16" spacing="1" margin="1" tilecount="476" columns="28">
+<tileset version="1.8" tiledversion="1.8.2" name="roguelikeDungeon_magenta" tilewidth="16" tileheight="16" spacing="1" tilecount="522" columns="29">
  <image source="roguelikeDungeon_magenta.png" trans="ff00ff" width="492" height="305"/>
 </tileset>

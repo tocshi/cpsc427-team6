5a35580da2d0ada3306abaeeedd603b35154429d
c0linw <41310013+c0linw@users.noreply.github.com>
2022-03-20 11:58:22 -0700

    Oops i forgot to set game start back to non-debug room

--- src/world_system.cpp
+++ src/world_system.cpp
@@ -762,8 +762,8 @@ void WorldSystem::handle_end_player_turn(Entity player) {
 void WorldSystem::spawn_game_entities() {
 
 	// Switch between debug and regular room
-	//std::string next_map = roomSystem.getRandomRoom(Floors::FLOOR1, true);
-	std::string next_map = roomSystem.getRandomRoom(Floors::DEBUG, true);
+	std::string next_map = roomSystem.getRandomRoom(Floors::FLOOR1, true);
+	//std::string next_map = roomSystem.getRandomRoom(Floors::DEBUG, true);
 	SpawnData spawnData = createTiles(renderer, next_map);
 
 	// create all non-menu game objects

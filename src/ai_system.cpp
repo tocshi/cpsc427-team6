7aca444b38cf601d0731422392d61261d5b37a60
cliff42 <chris.cliff@shaw.ca>
2022-04-20 20:47:21 -0700

    added slime a star toggle

--- src/ai_system.cpp
+++ src/ai_system.cpp
@@ -701,8 +701,8 @@ void AISystem::slime_logic(Entity slime, Entity& player) {
 			}
 			else {
 				std::vector<AstarNode*> starVector = AstarPathfinding(slime, chaseRange);
-
-				if (starVector.size() == 0) {
+				
+				if (starVector.size() == 0 || !useSlimeAStar) {
 					vec2 direction = simple_path_find(motion_struct.position, player_motion.position, slime);
 					motion_struct.velocity = slime_velocity * direction;
 					motion_struct.in_motion = true;
@@ -715,10 +715,10 @@ void AISystem::slime_logic(Entity slime, Entity& player) {
 					for (int i = 0; i < starVector.size(); i++) {
 						aStarMotion.path.push(starVector[i]->position);
 						// Uncomment the below to see the generated path
-						Entity test = createBigSlash(world.renderer, starVector[i]->position, 0, 0);
-						registry.renderRequests.get(test).used_texture = TEXTURE_ASSET_ID::ARTIFACT_PLACEHOLDER;
-						registry.motions.get(test).scale = vec2(50.f, 50.f);
-						registry.expandTimers.get(test).counter_ms = 20000;
+						//Entity test = createBigSlash(world.renderer, starVector[i]->position, 0, 0);
+						//registry.renderRequests.get(test).used_texture = TEXTURE_ASSET_ID::ARTIFACT_PLACEHOLDER;
+						//registry.motions.get(test).scale = vec2(50.f, 50.f);
+						//registry.expandTimers.get(test).counter_ms = 20000;
 						if (i == 1) {
 							motion_struct.destination = starVector[i]->position;
 							aStarMotion.currentDest = starVector[i]->position;
